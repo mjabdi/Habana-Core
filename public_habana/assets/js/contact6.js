43678f90a962a82a839ae185356660367862d46e
mjabdi <m.jafarabdi@gmail.com>
2021-10-19 00:09:31 +0330

    update

--- public_habana/assets/js/contact6.js
+++ public_habana/assets/js/contact6.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(el) {
-    var flipdown = new FlipDown(1633108038, {
+    var flipdown = new FlipDown(1638378438, {
         theme: 'light' // or dark
     });
     flipdown.start();
